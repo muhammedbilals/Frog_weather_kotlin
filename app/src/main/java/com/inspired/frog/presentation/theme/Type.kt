5fb11f05cbf46dec080406eb112a017419d2b485
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-06-18 18:56:07 +0530

    added custom fonts

--- app/src/main/java/com/inspired/frog/presentation/theme/Type.kt
+++ app/src/main/java/com/inspired/frog/presentation/theme/Type.kt
@@ -4,10 +4,32 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.inspired.frog.R
 
+
+val Roboto = FontFamily(
+    Font(R.font.noto_sans_mono_black, FontWeight.Black, FontStyle.Normal),
+
+    Font(R.font.noto_sans_mono_bold, FontWeight.Bold, FontStyle.Normal),
+
+    Font(R.font.noto_sans_mono_extra_bold, FontWeight.ExtraBold, FontStyle.Normal),
+
+    Font(R.font.noto_sans_mono_light, FontWeight.Light, FontStyle.Normal),
+
+    Font(R.font.noto_sans_mono_medium, FontWeight.Medium, FontStyle.Normal),
+
+    Font(R.font.noto_sans_mono_regular, FontWeight.Normal, FontStyle.Normal),
+
+    Font(R.font.noto_sans_mono_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+
+    Font(R.font.noto_sans_mono_thin, FontWeight.Thin, FontStyle.Normal),
+
+
+
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
