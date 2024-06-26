6c55611362d58a3b4bcc9d0c2f20112d9e6d21c7
Muhammed Bilal <bilalmuhammed363@gmail.com>
2024-06-26 23:54:00 +0530

    fetched user location with lat long

--- app/src/main/java/com/inspired/frog/presentation/MainActivity.kt
+++ app/src/main/java/com/inspired/frog/presentation/MainActivity.kt
@@ -2,8 +2,11 @@ package com.inspired.frog.presentation
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.location.Geocoder
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -21,6 +24,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +32,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.inspired.frog.presentation.composibles.CurrentWeather
 import com.inspired.frog.presentation.composibles.WeatherCard
 import com.inspired.frog.presentation.composibles.WeatherForecast
@@ -35,20 +41,49 @@ import com.inspired.frog.presentation.theme.Black
 import com.inspired.frog.presentation.theme.FrogWeatherAppTheme
 import com.inspired.frog.presentation.theme.Roboto
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.IOException
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel: WeatherViewModel by viewModels()
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     @OptIn(ExperimentalMaterial3Api::class)
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "MissingPermission")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val locationName = mutableStateOf("")
+
         permissionLauncher = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    // Got last known location. In some rare situations this can be null.
+                    if(location!=null){
+                        Log.d("location",location.toString())
+                        val geocoder = Geocoder(this, Locale.getDefault())
+                        try {
+                            val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+                            if (addresses != null) {
+                                if (addresses.isNotEmpty()) {
+                                    val address = addresses[0]
+                                     locationName.value = address.locality // You can also use address.locality, address.adminArea, etc.
+                                    Log.d("location", "Location Name: ${locationName.value}")
+                                } else {
+                                    Log.d("location", "No address found for location")
+                                }
+                            }
+                        } catch (e: IOException) {
+                            Log.d("location", "Geocoder failed", e)
+                        }
+                    }
+                }
             viewModel.loadWeatherInfo()
         }
         permissionLauncher.launch(
@@ -57,13 +92,15 @@ class MainActivity : ComponentActivity() {
                 Manifest.permission.ACCESS_COARSE_LOCATION,
             )
         )
+         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         enableEdgeToEdge()
         setContent {
             FrogWeatherAppTheme {
               Scaffold(
-//top appbar
+                  //top appbar
                   topBar = {
-                      CenterAlignedTopAppBar(title = { Text(text = "Dubai", style =  MaterialTheme.typography.titleMedium )})
+                      CenterAlignedTopAppBar(title = { Text(text = locationName.value, style =  MaterialTheme.typography.titleMedium )})
                   },
                   containerColor = MaterialTheme.colorScheme.background,
                   modifier = Modifier.fillMaxSize()
@@ -76,9 +113,9 @@ class MainActivity : ComponentActivity() {
                     ) {
                         item { Spacer(modifier = Modifier.height(150.dp)) }
                         item { WeatherCard(state = viewModel.state) }
-
-
                         item { CurrentWeather(state = viewModel.state) }
+                        item { Spacer(modifier = Modifier.height(40.dp)) }
+                        
                         item {
                             if (viewModel.state.isLoading) {
                                 Box(
